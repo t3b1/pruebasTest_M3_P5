8d1c1bf84f0c9888c87c47ef466ffabdc945ad1c
esteban <teba.gonzalez.eg@gmail.com>
2022-06-22 12:49:00 -0400

    probando test completo

---
+++ 00-ProbandoTests/test.js
@@ -0,0 +1,18 @@
+function saludar (nombre){
+    if (nombre === undefined){
+        return 'Hola!'
+    }
+    else {
+        return "Hola, " + nombre + "!";
+    }
+}
+saludar()
+
+
+function sumar(num, num2){
+    return  valor = num + num2;
+
+}
+
+sumar(4,5);
+sumar(7,8)
